38e96ef0058d88bd424c1542441a2a1d489fe194
evan <evan.yu6748@gmail.com>
2025-02-05 21:45:54 -0500

    some clean up

--- lib/SchedulingLandingScreen.dart
+++ lib/SchedulingLandingScreen.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutterdb/firestore/firestore_stream.dart';
 
 //
 class SchedulingLandingScreen extends StatelessWidget {
   final String documentId;
-
-  const SchedulingLandingScreen({super.key, required this.documentId});
+  final FirestoreService firestoreService =
+      FirestoreService(); // Instantiate service | could just do it in build
+  SchedulingLandingScreen({super.key, required this.documentId});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Scheduling')),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('homeassistant')
-            .doc(documentId)
-            .snapshots(),
+      body: StreamBuilder(
+        //get stream
+        stream: firestoreService.getDocumentStream(documentId),
         builder: (context, snapshot) {
           if (!snapshot.hasData) return CircularProgressIndicator();
-
           var data = snapshot.data!.data() as Map<String, dynamic>;
           var status = data['status'] ?? 'No Status';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
+import 'package:flutterdb/homeassistant/ha_start.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart'; // new
 import 'app_state.dart'; // new
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-// Home Assistant Imports
-import 'package:home_assistant/home_assistant.dart';
-import 'src/home_assistant_config.dart'; // new
 import 'router/app_router.dart'; // Import your new router file
 
 void main() {
   print("I'M ALIVE");
-  //Home Assistant Inits
-  final HomeAssistant homeAssistant = HomeAssistant(
-      baseUrl: HomeAssistantConfig.baseUrl,
-      bearerToken: HomeAssistantConfig.token);
+  //Moved Home Assistant Inits to ha_start.dart
   fetchData(homeAssistant);
-
   WidgetsFlutterBinding.ensureInitialized();
   //Provider: ChangeNotifierProvider is responsible for init the app state & for state management
   runApp(ChangeNotifierProvider(
@@ -24,31 +18,6 @@ void main() {
     //call back interface to parent widget to return a child of that widget i.e ChangeNotifierProvider
     builder: ((context, child) => const MyApp()),
   ));
-}
-
-fetchData(HomeAssistant homeAssistant) async {
-  print("The API is working: ${await homeAssistant.verifyApiIsWorking()}");
-
-  //final Configuration config = await homeAssistant.fetchConfig();
-  //print(config.toJson());
-
-  //final List<Entity> entities = await homeAssistant.fetchStates();
-  //print(entities.);
-
-  final Entity entity =
-      await homeAssistant.fetchState("light.esphome_web_60beb8_test_light");
-  print(entity.attributes.brightness);
-
-  //final List<Service> services = await homeAssistant.fetchServices();
-  //print(services.first.domain);
-
-  //Dummy Services
-  homeAssistant.executeService("light.esphome_web_60beb8_test_light", "turn_on",
-      additionalActions: {
-        "brightness": 50,
-      });
-
-  await Future.delayed(Duration(seconds: 1));
 }
 
 class MyApp extends StatelessWidget {
